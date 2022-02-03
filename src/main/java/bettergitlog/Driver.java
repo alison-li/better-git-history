4790aa1ad05594c596c1a5eed9595b95303d9b92
Alison Li <al260708761@gmail.com>
2022-02-03 10:51:59 -0800

    Reduce lines in `getFileFromCommit` and rework `getFileLog` to return a map of commits

--- src/main/java/bettergitlog/Driver.java
+++ src/main/java/bettergitlog/Driver.java
@@ -4,18 +4,17 @@ import bettergitlog.jgit.JGit;
 import org.eclipse.jgit.revwalk.RevCommit;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.Map;
 
 public class Driver {
     public static void main(String[] args) throws IOException {
         JGit jgit = new JGit("../kafka");
         String fileName = "streams/src/main/java/org/apache/kafka/streams/KafkaStreams.java";
-        List<String> fileNames = jgit.getAllOldFilePaths(fileName);
-        for (String s : fileNames) {
-            System.out.println(s);
+        Map<RevCommit, String> commitMap = jgit.getFileLog(fileName);
+        for (Map.Entry entry : commitMap.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
         }
-        List<RevCommit> commitList = jgit.getFileLog(fileName);
-        System.out.println(commitList.size());
-        jgit.getFileFromCommit(commitList.get(commitList.size() - 1), fileName);
+        System.out.println(commitMap.size());
+        jgit.getFileFromCommit(commitMap.keySet().stream().findFirst().get(), fileName);
     }
 }
--- src/main/java/bettergitlog/jgit/JGit.java
+++ src/main/java/bettergitlog/jgit/JGit.java
@@ -4,22 +4,18 @@ import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.diff.DiffEntry;
 import org.eclipse.jgit.diff.RenameDetector;
-import org.eclipse.jgit.errors.MissingObjectException;
 import org.eclipse.jgit.errors.RepositoryNotFoundException;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.ObjectLoader;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.revwalk.filter.RevFilter;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.eclipse.jgit.treewalk.TreeWalk;
-import org.eclipse.jgit.treewalk.filter.PathFilter;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class JGit {
     private final Repository repository;
@@ -40,91 +36,44 @@ public class JGit {
     }
 
     /**
-     * Retrieve the full Git history for a given file, including file renames.
+     * Retrieve the full Git history for a given file, including file renames/old paths.
      * @param filePath The file to get the full history for.
-     * @return The list of commits.
+     * @return A map with the commit mapped to the file path in that commit.
      */
-    public List<RevCommit> getFileLog(String filePath) {
-        List<RevCommit> commits = new ArrayList<>();
-        try {
-            List<String> allFilePaths = getAllOldFilePaths(filePath);
-            RevCommit startCommit = null;
-            for (String f : allFilePaths) {
-                Iterable<RevCommit> log = git.log()
-                        .addPath(f)
-                        .setRevFilter(RevFilter.NO_MERGES)
-                        .call();
-                for (RevCommit commit : log) {
-                    if (commits.contains(commit)) {
-                        startCommit = null;
-                    } else {
-                        startCommit = commit;
-                        commits.add(commit);
-                    }
-                }
-                if (startCommit == null) return commits;
-            }
-        } catch (GitAPIException e) {
-            System.out.println("Error while retrieving history for file.");
-        }
-        return commits;
-    }
-
-    /**
-     * Retrieve all of the old paths this file had if it was renamed or moved.
-     * @param filePath The initial file path to find old file paths for.
-     * @return A list of all old file paths, including the current file path.
-     */
-    public List<String> getAllOldFilePaths(String filePath) {
-        List<RevCommit> commits = new ArrayList<>();
-        List<String> oldFilePaths = new ArrayList<>();
+    public Map<RevCommit, String> getFileLog(String filePath) {
+        Map<RevCommit, String> commitMap = new LinkedHashMap<>();
         String updatedPath = filePath;
         try {
             RevCommit startCommit = null;
             do {
-                oldFilePaths.add(updatedPath);
                 Iterable<RevCommit> log = git.log()
                         .addPath(updatedPath)
                         .setRevFilter(RevFilter.NO_MERGES)
                         .call();
                 for (RevCommit commit : log) {
-                    if (commits.contains(commit)) {
+                    if (commitMap.containsKey(commit)) {
                         startCommit = null;
                     } else {
                         startCommit = commit;
-                        commits.add(commit);
+                        commitMap.put(commit, updatedPath);
                     }
                 }
-                if (startCommit == null) return oldFilePaths;
+                if (startCommit == null) return commitMap;
             }
             while ((updatedPath = getRenamedPath(startCommit, filePath)) != null);
         } catch (GitAPIException | IOException e) {
             System.out.println("Error while collecting fileNames.");
         }
-        return oldFilePaths;
+        return commitMap;
     }
 
     public void getFileFromCommit(RevCommit commit, String filePath) {
-        List<String> oldFilePaths = getAllOldFilePaths(filePath);
-        try (RevWalk revWalk = new RevWalk(repository)) {
-            try (TreeWalk treeWalk = new TreeWalk(repository)) {
-                treeWalk.addTree(commit.getTree());
-                treeWalk.setRecursive(true);
-                // Try all file paths associated with the given file path
-                for (String path : oldFilePaths) {
-                    treeWalk.setFilter(PathFilter.create(path));
-                    if (treeWalk.next()) break;
-                }
-                if (!treeWalk.next()) {
-                    throw new IllegalStateException("Did not find expected file: " + filePath);
-                }
-                ObjectId objectId = treeWalk.getObjectId(0);
-                ObjectLoader loader = repository.open(objectId);
-                loader.copyTo(System.out); // TODO: What to store this in and return?
-            } catch (IOException e) {
-                System.out.println("Error while looking for file in commit");
-            }
-            revWalk.dispose();
+        try (TreeWalk treeWalk = TreeWalk.forPath(repository, filePath, commit.getTree())) {
+            ObjectId objectId = treeWalk.getObjectId(0);
+            ObjectLoader loader = repository.open(objectId);
+            loader.copyTo(System.out); // TODO: What to store this in and return?
+        } catch (IOException e) {
+            System.out.println("Error while looking for file in commit");
         }
     }
 
@@ -133,11 +82,8 @@ public class JGit {
      * Source: https://stackoverflow.com/questions/11471836/how-to-git-log-follow-path-in-jgit-to-retrieve-the-full-history-includi
      * @param startCommit The first commit in the range to look at.
      * @return String or null
-     * @throws IOException
-     * @throws MissingObjectException
-     * @throws GitAPIException
      */
-    private String getRenamedPath(RevCommit startCommit, String initialPath) throws IOException, MissingObjectException, GitAPIException {
+    private String getRenamedPath(RevCommit startCommit, String initialPath) throws IOException, GitAPIException {
         Iterable<RevCommit> allCommitsLater = git.log()
                 .add(startCommit)
                 .setRevFilter(RevFilter.NO_MERGES)
