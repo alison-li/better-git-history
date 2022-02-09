264d15ba8ceec749f3a9f5011ec1acaf71f72f44
Alison Li <al260708761@gmail.com>
2022-02-09 10:41:09 -0800

    Update ChangeDistiller methods and include commented code in case

--- src/main/java/bettergithistory/BetterGitHistory.java
+++ src/main/java/bettergithistory/BetterGitHistory.java
@@ -1,6 +1,6 @@
 package bettergithistory;
 
-import bettergithistory.distiller.Distiller;
+import bettergithistory.extractors.Distiller;
 import bettergithistory.jgit.JGit;
 import bettergithistory.util.FileUtil;
 import ch.uzh.ifi.seal.changedistiller.model.entities.SourceCodeChange;
@@ -39,38 +39,76 @@ public class BetterGitHistory {
      * Retrieve a filtered version of the commit history using ChangeDistiller.
      * @return The filtered commit history.
      */
-    public Map<RevCommit, String> getFilteredCommitHistory() {
+    public Map<RevCommit, String> getChangeDistillerCommitHistory() {
+        if (this.commitMap.size() <= 2) return commitMap;
+
         Map<RevCommit, String> filteredCommitMap = new LinkedHashMap<>();
         List<RevCommit> commits = new ArrayList<>(commitMap.keySet());
         for (int verNum = 0; verNum < this.commitMap.size() - 1; verNum++) {
-            File left = new File(String.format("out/ver%d.java", verNum));
-            File right = new File(String.format("out/ver%d.java", verNum + 1));
+            int leftVer = verNum;
+            int rightVer = verNum + 1;
+            File left = new File(String.format("out/ver%d.java", leftVer));
+            File right = new File(String.format("out/ver%d.java", rightVer));
             List<SourceCodeChange> changes = Distiller.extractSourceCodeChanges(left, right);
             if (!changes.isEmpty()) {
+                System.out.printf("HIT VER %d -> VER %d%n", leftVer, rightVer);
                 // Keep both commits in the history since the change list is non-empty between the two versions.
-                RevCommit firstCommit = commits.get(verNum);
+                RevCommit firstCommit = commits.get(leftVer);
                 String firstPath = this.commitMap.get(firstCommit);
-                RevCommit secondCommit = commits.get(verNum + 1);
+                RevCommit secondCommit = commits.get(rightVer);
                 String secondPath = this.commitMap.get(secondCommit);
                 filteredCommitMap.put(firstCommit, firstPath);
                 filteredCommitMap.put(secondCommit, secondPath);
             }
         }
+
         return filteredCommitMap;
     }
+
+    /**
+     * Removed for now due to few results using ChangeDistiller.
+     * Two pointer approach to comparing files.
+     */
+//    public Map<RevCommit, String> getFilteredCommitHistoryWithFileHop() {
+//        if (this.commitMap.size() <= 2) return commitMap;
+//
+//        Map<RevCommit, String> filteredCommitMap = new LinkedHashMap<>();
+//        List<RevCommit> commits = new ArrayList<>(this.commitMap.keySet());
+//        int leftVer = 0;
+//        int rightVer = 1;
+//        while (rightVer < this.commitMap.size()) {
+//            File left = new File(String.format("out/ver%d.java", leftVer));
+//            File right = new File(String.format("out/ver%d.java", rightVer));
+//            List<SourceCodeChange> changes = Distiller.extractSourceCodeChanges(left, right);
+//            if (!changes.isEmpty()) {
+//                System.out.printf("HIT VER %d -> VER %d%n", leftVer, rightVer);
+//                // Keep both commits in the history since the change list is non-empty between the two versions.
+//                RevCommit firstCommit = commits.get(leftVer);
+//                String firstPath = this.commitMap.get(firstCommit);
+//                RevCommit secondCommit = commits.get(rightVer);
+//                String secondPath = this.commitMap.get(secondCommit);
+//                filteredCommitMap.put(firstCommit, firstPath);
+//                filteredCommitMap.put(secondCommit, secondPath);
+//                leftVer = rightVer;
+//            }
+//            rightVer++;
+//        }
+//
+//        return filteredCommitMap;
+//    }
 
     /**
      * Retrieve all of the source code changes performed in a file.
      * @return A chronological list of the source code change operations performed.
      */
-    public List<List<SourceCodeChange>> getAllSourceCodeChanges() {
+    public List<List<SourceCodeChange>> getAllChangeDistillerSourceCodeChanges() {
         List<List<SourceCodeChange>> allSourceCodeChanges = new ArrayList<>();
         // This will give us a chronological list of source code changes since the commit map
         // is ordered from most recent commit to oldest.
         // We iterate backwards starting from the oldest file version to see the subsequent changes applied.
-        for (int verNum = this.commitMap.size() - 1; verNum > 0; verNum--) {
+        for (int verNum = 0; verNum < this.commitMap.size(); verNum++) {
             File left = new File(String.format("out/ver%d.java", verNum));
-            File right = new File(String.format("out/ver%d.java", verNum - 1));
+            File right = new File(String.format("out/ver%d.java", verNum + 1));
             List<SourceCodeChange> changes = Distiller.extractSourceCodeChanges(left, right);
             if (!changes.isEmpty()) {
                 allSourceCodeChanges.add(changes);
